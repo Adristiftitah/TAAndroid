30f56109544c7fe0e2c1f3847d43ad36e759383c
adristi <adristi@gmail.com>
2017-06-13 13:57:00 +0700

    Done

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl602/taandroid/Model/ResultResponse.java
@@ -0,0 +1,8 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl602.taandroid.Model;
+
+import java.util.List;
+
+public class ResultResponse {
+    public int page;
+    public List<Result> results;
+}
